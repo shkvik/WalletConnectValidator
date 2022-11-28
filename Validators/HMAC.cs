ae97d738c7a61583c35df51030f61b77389178a7
pendulum <75574213+shkvik@users.noreply.github.com>
2022-11-28 19:53:09 +0300

    Add project files.

---
+++ Validators/HMAC.cs
@@ -0,0 +1,39 @@
+﻿using WalletConnectSharp.Core.Models;
+using WalletConnectSharp.Core.Network;
+using WalletConnectSharp.Core.Utils;
+
+namespace WalletConnectValidator.Validators
+{
+    static class HMAC
+    {
+        private static ICipher Cipher = new AESCipher();
+
+        public static async Task<bool> Decrypte()
+        {
+            Console.Write("Input iv:");
+            var c_iv = Console.ReadLine();
+
+            Console.Write("Input hmac:");
+            var c_hmac = Console.ReadLine();
+
+            Console.Write("Input data:");
+            var c_data = Console.ReadLine();
+
+            Console.Write("Input key:");
+            var c_key = Console.ReadLine();
+
+            var encryptedPayload = new EncryptedPayload()
+            {
+                iv = c_iv,
+                hmac = c_hmac,
+                data = c_data
+            };
+
+            byte[] key = c_key.HexToByteArray();
+
+            var result = await Cipher.DecryptWithKey(key, encryptedPayload);
+
+            return true;
+        }
+    }
+}
---
+++ WalletConnectValidator.cs
@@ -0,0 +1,19 @@
+﻿using WalletConnectValidator.Validators;
+
+namespace WalletConnectValidator;
+
+public static class WalletConnectValidator 
+{
+
+    public delegate Task<bool> CryptopMethod();
+
+    private static Dictionary<string, CryptopMethod>? _methodsDictionary = new Dictionary<string, CryptopMethod>()
+    {
+        { "hmac", delegate { return HMAC.Decrypte(); }  }
+    };
+
+    private static async Task Main(string[] args)
+    {
+        var test = _methodsDictionary["hmac"].Invoke();
+    }
+}
